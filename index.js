768bbbd74807344adbf185f868859662345340d2
piyush goyal <piyush2022ug@gmail.com>
2025-05-18 21:53:04 +0530

    fix: update server port to 4002 and add dev script to package.json

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const Post = require("./models/post.model");
 const Comment = require("./models/comment.model");
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 4002;
 
 app.use(express.json());
 app.use(
