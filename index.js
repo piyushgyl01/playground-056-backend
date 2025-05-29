38c188a7541156e61734cefbac0a91fa87d34010
piyush goyal <piyush2022ug@gmail.com>
2025-05-29 21:01:56 +0530

    fix: update CORS configuration to include localhost:5175

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.use(
   cors({
     origin: [
       "https://playground-054-frontend.vercel.app",
-      "http://localhost:5174",
+      "http://localhost:5173",
+      "http://localhost:5175",
     ],
     credentials: true,
     optionsSuccessStatus: 200,
